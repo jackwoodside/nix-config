d5760a427a2acc38fd9781f63bf00e319d820383
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2024-08-02 23:09:06 +1000

    Git merge option

--- programs/common/git.nix
+++ programs/common/git.nix
@@ -5,5 +5,8 @@
     enable = true;
     userEmail = "20267593+jackwoodside@users.noreply.github.com";
     userName = "jackwoodside";
+    extraConfig = {
+      pull.rebase = false;
+    };
   };
 }
