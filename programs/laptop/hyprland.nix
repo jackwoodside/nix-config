9bd022cd9eb46b970ba06a5dbf32a02f5355d2cd
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2024-08-03 16:37:22 +1000

    Fixed laptop screen scaling

--- programs/laptop/hyprland.nix
+++ programs/laptop/hyprland.nix
@@ -3,8 +3,8 @@
 {
   wayland.windowManager.hyprland = {
     extraConfig = ''
-      monitor=eDP-1,1920x1080@60,960x2160,1
-      monitor=DP-1,3840x2160@60,0x0,1
+      monitor=eDP-1,1920x1080@60,320x1440,1
+      monitor=DP-1,3840x2160@60,0x0,1.5
     '';
   };
 }
