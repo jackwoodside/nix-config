fa4a71dcb9499b2763e44e89d1ff4a8128b78559
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2024-08-04 17:20:48 +1000

    Configured hypridle

--- programs/common/hypridle.nix
+++ programs/common/hypridle.nix
@@ -1,26 +1,41 @@
 { pkgs, ... }:
 
 {
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
-        ignore_dbus_inhibit = false;
-        lock_cmd = "${pkgs.hyprlock}/bin/hyprlock";
-      };
+  services.hypridle =
+    let
+      hyprctl = "${pkgs.hyprland}/bin/hyprctl";
+      hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
+    in
+    {
+      enable = true;
+      settings = {
+        general = {
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "${hyprctl} dispatch dpms on";
+          ignore_dbus_inhibit = false;
+          lock_cmd = "pidof ${hyprlock} || ${hyprlock}";
+        };
 
-      listener = [
-        {
-          timeout = 300;
-          on-timeout = "${pkgs.hyprlock}/bin/hyprlock";
-        }
-        {
-          timeout = 600;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-      ];
+        listener = [
+          # After 5 minutes, lock device
+          {
+            timeout = 300;
+            on-timeout = "loginctl lock-session";
+          }
+
+          # After 5.5 minutes, screen off
+          {
+            timeout = 330;
+            on-timeout = "${hyprctl} dispatch dpms off";
+            on-resume = "${hyprctl} dispatch dpms on";
+          }
+
+          # After 30 minutes, suspend device
+          {
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
+          }
+        ];
+      };
     };
-  };
 }
