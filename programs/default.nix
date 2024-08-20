37c0d251464799431aa65459736b40548b8baee0
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2024-08-20 10:55:21 +1000

    Added basic lf config

--- programs/default.nix
+++ programs/default.nix
@@ -17,6 +17,7 @@
     ./modules/hyprland.nix
     ./modules/hyprlock.nix
     ./modules/kitty.nix
+    ./modules/lf
     ./modules/mpv.nix
     ./modules/networkmanager-dmenu.nix
     ./modules/nix-index.nix
