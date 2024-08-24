4fe6b90d4bb1b384ccd35c217da547b5f94c7ecb
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2024-08-24 15:15:57 +1000

    Monitor stuff

--- programs/desktop-home/hyprland.nix
+++ programs/desktop-home/hyprland.nix
@@ -5,7 +5,7 @@
     monitor = [
       "HDMI-A-1,1920x1080@60,0x0,1"
       "DP-1,1920x1080@60,1920x0,1"
-      "Unknown-1,disable"
+      "Unknown-1,disable" # Fake monitor from NVidia driver bug
     ];
   };
 }
