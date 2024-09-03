c4d733223a2f8d29062ee591e3652c3b9b319164
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2024-09-03 10:06:32 +1000

    Enabled SSH on desktop-work

--- systems/desktop-work/default.nix
+++ systems/desktop-work/default.nix
@@ -9,6 +9,12 @@
   # Networking
   networking.hostName = "io";
 
+  # SSH
+  services.openssh = {
+    enable = true;
+    settings.X11Forwarding = true;
+  };
+
   # Users
   home-manager.users.jack = import ../../programs/desktop-work;
 }
