58bd4f63b338c6953dde3ffef95ae84cfede42b6
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2025-02-14 10:21:39 +1100

    Updated sioyek config

--- programs/modules/sioyek.nix
+++ programs/modules/sioyek.nix
@@ -18,15 +18,16 @@ in
     };
     config = {
       "background_color" = colours.base00;
-      "touchpad_sensitivity" = "0.4";
-      "page_separator_width" = "4";
+      "inverse_search_command" = "nvr --remote \"%1\" +%2";
+      "overview_size" = "0.85 0.85";
       "page_separator_color" = colours.base00;
+      "page_separator_width" = "4";
       "search_url_g" = "https://www.google.com/search?q=";
       "should_launch_new_window" = "1";
       "should_use_multiple_monitors" = "0";
-      "overview_size" = "0.85 0.85";
-      "inverse_search_command" = "nvr --remote \"%1\" +%2";
+      "show_document_name_in_statusbar" = "1";
       "synctex_highlight_color" = "0.0 0.0 0.0";
+      "touchpad_sensitivity" = "0.4";
     };
   };
 }
