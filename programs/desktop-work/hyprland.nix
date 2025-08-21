6a529b8d075801dce3f879dec3aa2060099207ac
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2025-08-21 11:19:19 +1000

    Moved desktop-work monitors

--- programs/desktop-work/hyprland.nix
+++ programs/desktop-work/hyprland.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland.settings = {
     monitor = [
       "DP-1,3840x2160@60,1920x0,1.5"
-      "HDMI-A-1,1920x1080@60,0x360,1"
+      "HDMI-A-1,1920x1080@60,0x300,1"
     ];
   };
 }
