2ae9b1c467c557355f5641224b284dd15821d763
jackwoodside <20267593+jackwoodside@users.noreply.github.com>
2026-08-17 19:05:53 +1000

    Fixed grim error

--- programs/modules/flameshot.nix
+++ programs/modules/flameshot.nix
@@ -5,9 +5,11 @@
     enable = true;
     settings = {
       General = {
+        disabledGrimWarning = true;
         disabledTrayIcon = true;
         showHelp = false;
         showStartupLaunchMessage = false;
+        useGrimAdapter = true;
       };
     };
   };
